9a4e231bdf73f10b91c77b99f3788bbb480b3249
yulimmm <89307538+yulimmm@users.noreply.github.com>
2024-11-10 14:23:33 +0900

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -2,6 +2,6 @@ import sys
 
 number = int(sys.argv[1])
 
-for i in range(1,number): #loop between 1 and number
+for i in range(1,number+1): #loop between 1 and number
     if number%i==0:
         print(i, end=" ")
